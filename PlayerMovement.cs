78b50d0ec2a6ce375945825cd3af15f6af812116
Ahmad <a7mad7ussain777@gmail.com>
2021-01-05 23:31:39 +0300

    movement

---
+++ PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    private Animator myAnimator;
+
+    public float speed = 2.0f;
+    public float horizMovement;
+    
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        horizMovement = Input.GetAxisRaw("Horizontal");
+        Fixedupdate();
+    }
+
+    private void Fixedupdate()
+    {
+        rb2D.velocity = new Vector2 (horizMovement * speed, rb2D.velocity.y);
+    }
+
+}
