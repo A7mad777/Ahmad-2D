7e75856f56c97e5da994465c8b6ca37d3bf82908
Ahmad <a7mad7ussain777@gmail.com>
2021-01-06 17:07:31 +0300

    JumpingCode-V2

--- PlayerJump.cs
+++ PlayerJump.cs
@@ -4,28 +4,54 @@ using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
 {
-    [Header("Public Vars")]
+    [Header("Jump Details")]
     [SerializeField] public float jumpForce;
-    [SerializeField] public bool grounded;
-    private Rigidbody2D rb;
+    public float jumpTime;
+    private float jumpTimeCounter;
+    private bool stoppedjumping;
+    
 
-    [Header("Private Vars")]
+    [Header("Ground Details")]
     [SerializeField] private Transform groundcheck;
     [SerializeField] private float radOCircle;
-    [SerializeField] private LayerMask whatIsGround; 
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] public bool grounded;
+
+    [Header("Component")]
+    private Rigidbody2D rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        jumpTimeCounter = jumpTime;
     }
 
     private void Update()
     {
         grounded = Physics2D.OverlapCircle(groundcheck.position, radOCircle, whatIsGround);
 
+        if(grounded)
+        {
+            jumpTimeCounter = jumpTime;
+        }
+
         if(Input.GetButtonDown("Jump") && grounded)
         {
+            //Jmping
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            stoppedjumping = false;
+        }
+
+        if(Input.GetButton("Jump") && !stoppedjumping && (jumpTimeCounter > 0))
+        {
+            //MidJump
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpTimeCounter -= Time.deltaTime;
+        }
+        if(Input.GetButtonUp("Jump"))
+        {
+            jumpTimeCounter = 0;
+            stoppedjumping = true;
         }
     }
 
