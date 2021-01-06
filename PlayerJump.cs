f156bc3e6e7c849bbeb1581b156a4385dff864a4
Ahmad <a7mad7ussain777@gmail.com>
2021-01-06 15:31:08 +0300

    JumpingCode
    jump-V1

---
+++ PlayerJump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [Header("Public Vars")]
+    [SerializeField] public float jumpForce;
+    [SerializeField] public bool grounded;
+    private Rigidbody2D rb;
+
+    [Header("Private Vars")]
+    [SerializeField] private Transform groundcheck;
+    [SerializeField] private float radOCircle;
+    [SerializeField] private LayerMask whatIsGround; 
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        grounded = Physics2D.OverlapCircle(groundcheck.position, radOCircle, whatIsGround);
+
+        if(Input.GetButtonDown("Jump") && grounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(groundcheck.position, radOCircle);
+    }
+}
