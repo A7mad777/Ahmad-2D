d550a2bf2ff2c6230ef7f60aa1cba84b50c82317
Ahmad <a7mad7ussain777@gmail.com>
2021-01-08 02:45:50 +0300

    EnemyCode_V1
    Not Finished...

---
+++ Enemy1/EnemyWalk.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWalk : MonoBehaviour
+{
+    private enum State
+    {
+        Walking,
+        Knockback,
+        Dead
+    }
+
+    private State currentState;
+
+    [SerializeField]
+    private float
+        groundCheckDistance,
+        wallCheckDistance,
+        movementSpeed,
+        maxHealth;
+
+    [SerializeField]
+    private Transform
+        groundCheck,
+        wallCheck;
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+    private float currentHealth;
+
+    private int facingDirection;
+
+    private Vector2 movement;
+
+    private bool
+        groundDetected,
+        WallDetected;
+
+    private GameObject alive;
+    private Rigidbody2D aliveRb;
+
+    private void Start()
+    {
+        alive = transform.Find("Alive").gameObject;
+        aliveRb = alive.GetComponent<Rigidbody2D>();
+
+        facingDirection = 1;
+    }
+
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case State.Walking:
+                UpdateWalkingState();
+                break;
+            case State.Knockback:
+                UpdateKnockbackState();
+                break;
+            case State.Dead:
+                UpdateDeadState();
+                break;
+        }
+    }
+
+    //WALKING-----------------------------------------------------------
+
+    private void EnterWalkingState()
+    {
+
+    }
+
+    private void UpdateWalkingState()
+    {
+        groundDetected = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+        WallDetected = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
+
+        if(!groundDetected || WallDetected)
+        {
+            Flip();
+        }
+        else
+        {
+            movement.Set(movementSpeed * facingDirection, aliveRb.velocity.y);
+            aliveRb.velocity = movement;
+        }
+    }
+
+    private void ExitWalkingState()
+    {
+
+    }
+
+    //KNOCKBACK-----------------------------------------------------------
+
+    private void EnterKnockbackState()
+    {
+
+    }
+
+    private void UpdateKnockbackState()
+    {
+
+    }
+
+    private void ExitKnockbackState()
+    {
+
+    }
+
+    //DEAD----------------------------------------------------------------
+
+    private void EnterDeadState()
+    {
+
+    }
+
+    private void UpdateDeadState()
+    {
+
+    }
+
+    private void ExitDeadState()
+    {
+
+    }
+
+    //OTHER----------------------------------------------------------------
+
+    private void Flip()
+    {
+        facingDirection *= -1;
+        alive.transform.Rotate(0.0f, 100.0f, 0.0f);
+    }
+
+    private void SwitchState(State state)
+    {
+        switch(currentState)
+        {
+            case State.Walking:
+                ExitWalkingState();
+                break;
+            case State.Knockback:
+                ExitKnockbackState();
+                break;
+            case State.Dead:
+                ExitDeadState();
+                break;
+
+        }
+
+        switch (state)
+        {
+            case State.Walking:
+                EnterWalkingState();
+                break;
+            case State.Knockback:
+                EnterKnockbackState();
+                break;
+            case State.Dead:
+                EnterDeadState();
+                break;
+
+        }
+
+        currentState = state;
+    }
+}
